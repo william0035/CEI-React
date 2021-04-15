22f3af36823785144a073e1ff3efac916c35eef4
Tarcísio Madureira <tarcisiorio11@live.com>
2021-04-14 21:46:58 -0300

    Create LayoutMenu.js

---
+++ assets/components/LayoutMenu.js
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import React from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { css } from '../../assets/css/style';
+
+
+export default function LayoutMenu(props)
+{
+    async function logout()
+    {
+        await AsyncStorage.clear();
+        props.navigation.navigate('Home')
+    }
+return(
+<View style={css.area__menu}>
+    <TouchableOpacity style={css.button__home} onPress={()=>props.navigation.navigate('Home')}>
+        <Icon name="home" size={20} color='#fff'/>
+    </TouchableOpacity>
+
+    <Text style={css.area__title}>{props.title}</Text>
+
+    <TouchableOpacity style={css.button__logout} onPress={()=>logout()}>
+        <Icon name="sign-out" size={20} color='#fff'/>
+    </TouchableOpacity>
+</View>
+);
+}
