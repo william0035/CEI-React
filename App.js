1d43e980f7424fc3b929bfe33ec78d009141e901
William Sodré <71849007+william0035@users.noreply.github.com>
2021-04-14 22:34:01 -0300

    Conclusão

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+import {css} from './assets/css/style';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {Home, Login} from './view';
+import Menu from "./view/AreaRestrita/Menu";
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          title:"CEI: Controle de Estoque",
+          headerStyle:{backgroundColor:"#011670"},
+          headerTintColor:'#fff',
+          headerTitleStyle:{fontWeight:'bold'}
+          }}
+        />
+        <Stack.Screen name="Login" component={Login} 
+          options={{headerShown:false}}
+        />
+       <Stack.Screen name="Menu" options={{headerShown:false}} component={Menu} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
