51fd56494a253e858572d3a6a11c90e6cccb2fc7
Alison Vitor <71190247+Alison561@users.noreply.github.com>
2021-04-14 22:01:38 -0300

    Alison

--- assets/components/TaskList/index.js
+++ assets/components/TaskList/index.js
@@ -15,17 +15,43 @@ export default function TaskList({data , handleDelete, edit }) {
             <Text style={css.task}>ITEM: {data.task}   QNT:{data.quant}</Text>
             
             <View style={css.iconList}>
-            <TouchableOpacity onPress={()=>setOpen2(true)}>
-                <Icon name="pencil" size={30} color="#fff"/>
-            </TouchableOpacity>
-            <Text>    </Text>
-            <TouchableOpacity onPress={()=>handleDelete(data)}>
-                <Icon name="trash" size={30} color="#fff"/>
-            </TouchableOpacity>
-            </View>    
-              
-         
+                <TouchableOpacity onPress={()=>setOpen2(true)}>
+                    <Icon name="pencil" size={30} color="#fff"/>
+                </TouchableOpacity>
+                <Text>    </Text>
+                <TouchableOpacity onPress={()=>handleDelete(data)}>
+                    <Icon name="trash" size={30} color="#fff"/>
+                </TouchableOpacity>
+            </View>
+            <Modal animationType="slide" transparent={false} visible={open2}>
+                <SafeAreaView style={css.modal}>
+                    <View style={css.modalHeader}>
+                        <TouchableOpacity onPress={()=> setOpen2(false)}>
+                            <Icon style={{marginLeft:5,marginRight:5}} name="arrow-left" size={35} color='#fff'/>
+                        </TouchableOpacity>
+                        <Text style={css.modalTitle}>Editar</Text>
+                    </View>
+                    <View>
+                        <TextInput
 
+                            style={css.inputCad}
+                            value={data.task}
+                            onChangeText={(item) => setInput2(item)}
+                        />
+                        <TextInput
+
+                            keyboardType = 'numeric'
+                            placeholder='QUANTIDADE:'
+                            style={css.inputCad}
+                            value={inputQnt2}
+                            onChangeText={(item) => setInputQNT2(item)}
+                        />
+                        <TouchableOpacity style={css.inputSubmit} onPress={()=> edit(data,inputQnt2)}>
+                            <Text>Editar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </SafeAreaView>
+            </Modal>
         </View>
         
     )
