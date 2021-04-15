d7794f841670fe5b2342051233338a40488b7f39
William Sodré <71849007+william0035@users.noreply.github.com>
2021-04-14 21:19:54 -0300

    Loginfinal

--- Login.js
+++ Login.js
@@ -6,7 +6,53 @@ import {css} from '../assets/css/style';
 
 export default function Login({navigation})
 {
-    
+     const [display, setDisplay]=useState('none');
+    const [nome, setNome]=useState('null');
+    const [senha, setSenha]=useState('null');
+    const [login, setLogin]=useState('false');
+
+    useEffect(()=>{
+        verifyLogin();
+    },[])
+
+    async function verifyLogin()
+    {
+        let response=await AsyncStorage.getItem('userData');
+        let json=await JSON.parse(response);
+        if (json !== null){
+            setNome(json.nome);
+            setSenha(json.senha);
+            setLogin(true);
+            await AsyncStorage.setItem('userData' , JSON.stringify(json));
+            navigation.navigate('Menu');
+        }
+    }
+
+    async function sendForm()
+    {
+        let response=await fetch('http://192.168.0.105:3000/login' , {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nome: nome,
+                senha: senha
+            })
+        });
+        let json=await response.json();  
+        if (json ==='erro'){
+            setDisplay('flex');
+            setTimeout(()=>{
+                setDisplay('none');
+            },2000);
+            await AsyncStorage.clear();
+        }else{
+            await AsyncStorage.setItem('userData' , JSON.stringify(json));
+            navigation.navigate('Menu');
+        }
+    }
 
     return(
         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={[css.container,css.bgColor,css.viewMenu]}>
